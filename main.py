76102adba6766e0e1622281075cd0083d959c5dc
Joseph Thomas <>
2021-04-09 15:59:18 +0000

    Print instructie/introductie tekst

---
+++ main.py
@@ -0,0 +1,11 @@
+def print_horizontal_line():
+  print("--------------------------")
+
+print("WELKOM BIJ GALGJE!")
+print_horizontal_line()
+print("Dit is een spel waarbij je")
+print("een woord moet raden door")
+print("allerlei letters te proberen.")
+print("Je hebt alleen vijf levens.")
+print("Wanneer je een foute letter raadt,")
+print("Verlies je een leven.")
