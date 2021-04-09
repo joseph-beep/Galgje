fcbdf163e2b7ef6422a0b41b1287acba8dc7dccc
Joseph Thomas <>
2021-04-09 16:20:07 +0000

    Fix probleem met meerdere dezelfde letters

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ import random
 def print_horizontal_line():
   print("--------------------------")
 
+def find_letter_index_all(text, letter):
+  found_letter_indices = []
+
+  counter = 0
+  for character in text:
+    if character == letter:
+      found_letter_indices.append(counter)
+    
+    counter += 1
+  
+  return found_letter_indices
+
 print("WELKOM BIJ GALGJE!")
 print_horizontal_line()
 print("Dit is een spel waarbij je")
@@ -37,4 +49,9 @@ while True:
   letter = input("Welk letter: ")[0].lower()
 
   if letter in lowercasedWord:
-    knownLetters[lowercasedWord.index(letter)] = word[lowercasedWord.index(letter)]
+    same_letter_indices = find_letter_index_all(lowercasedWord, letter)
+
+    for i in same_letter_indices:
+      knownLetters[i] = word[i]
+      
+  print(''.join(knownLetters))
